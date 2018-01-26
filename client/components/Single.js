e51d613d63187b7e244b0abbd88918ab6ece86be
Alex Pattison <alexlpattison@gmail.com>
2018-01-25 21:38:04 -0800

    Add simple PhotoGrid and Single components

---
+++ client/components/Single.js
@@ -0,0 +1,11 @@
+import React form 'react';
+
+export class Single extends React.Component {
+  render() {
+    return (
+      <div className="single-photo">
+        I'm single Baby
+      </div>
+    )
+  }
+};
