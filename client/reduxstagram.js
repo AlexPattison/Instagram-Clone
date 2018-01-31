38abaeba91f2c9fcc22afe4e2b34e9eab9474332
Alex Pattison <alexlpattison@gmail.com>
2018-01-30 22:37:06 -0800

    Link up the store with 'react-redux'
    The Provider API allows us to integrate our redux store with react

--- client/reduxstagram.js
+++ client/reduxstagram.js
@@ -1,19 +1,23 @@
 import React from 'react';
 import { render } from 'react-dom';
 import { Router, Route, IndexRoute, browserHistory } from 'react-router';
+import { Provider } from 'react-redux';
 
 import css from './styles/style.styl';
 import { Main } from './components/Main';
 import { Single } from './components/Single';
 import { PhotoGrid } from './components/PhotoGrid';
+import { history, store } from './store';
 
 const router = (
-  <Router history={browserHistory}>
-    <Route path="/" components={Main}>
-      <IndexRoute components={PhotoGrid}></IndexRoute>
-      <Route path="/view/:postId" components={Single}></Route>
-    </Route>
-  </Router>
+  <Provider store={store}>
+    <Router history={history}>
+      <Route path="/" components={Main}>
+        <IndexRoute components={PhotoGrid}></IndexRoute>
+        <Route path="/view/:postId" components={Single}></Route>
+      </Route>
+    </Router>
+  </Provider>
 )
 
 render(router, document.getElementById('root'));
